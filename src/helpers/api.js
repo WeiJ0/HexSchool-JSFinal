604fbd6c85842760d9db288465b89f5de118a0a6
Wystan Lin <weijlin850621@gmail.com>
2022-11-24 21:38:12 +0800

    建立API JS

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,1 +1,12 @@
 import axios from 'axios';
+
+const userRequest = axios.create({
+    baseURL: 'https://weij0-app.herokuapp.com/users/'
+});
+
+// 確認是否已註冊
+export const userEmailCheck = (email) => userRequest.post('/email', { email });
+// 註冊
+export const userSignUp = (data) => userRequest.post('/signup', data);
+// 登入
+export const userSignIn = (data) => userRequest.post('/signin', data);
